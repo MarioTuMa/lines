69de44c3c55bc5c5ad805f23c6322c80d52a1724
MarioTuMa <mtutuncumacias1@gmail.com>
2020-02-09 11:11:40 -0500

    concurrency of the medians

--- lines.py
+++ lines.py
@@ -5,9 +5,9 @@ fout.write("P3\n"+str(size)+" "+str(size)+"\n255\n")
 
 
 pixels = []
-for i in range(500):
+for i in range(size):
     pixels.append([])
-    for j in range(500):
+    for j in range(size):
         pixels[i].append([255,255,255])
 
 def line(x1,y1,x2,y2,r,g,b):
@@ -45,10 +45,25 @@ def line(x1,y1,x2,y2,r,g,b):
             extra += m
             shift=round(extra)
 
-for i in range(6):
-    line(random.randint(0,size-1),random.randint(0,size-1),random.randint(0,size-1),random.randint(0,size-1),random.randint(0,255),random.randint(0,255),random.randint(0,255))
-for i in range(500):
-    for j in range(500):
+p1x = random.randint(0,size-1)
+p1y = random.randint(0,size-1)
+p2x = random.randint(0,size-1)
+p2y = random.randint(0,size-1)
+p3x = random.randint(0,size-1)
+p3y = random.randint(0,size-1)
+
+line(p1x,p1y,p2x,p2y,0,0,0)
+line(p2x,p2y,p3x,p3y,0,0,0)
+line(p3x,p3y,p1x,p1y,0,0,0)
+
+line(p1x,p1y,int((p2x+p3x)/2),int((p2y+p3y)/2),128,128,128)
+line(p2x,p2y,int((p1x+p3x)/2),int((p1y+p3y)/2),128,128,128)
+line(p3x,p3y,int((p2x+p1x)/2),int((p2y+p1y)/2),128,128,128)
+
+
+
+for i in range(size):
+    for j in range(size):
         fout.write(str(pixels[i][j][0])+" "+str(pixels[i][j][1])+" "+str(pixels[i][j][2])+" ")
 
     fout.write("\n")
